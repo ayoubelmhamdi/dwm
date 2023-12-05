b6a0018fa3c3d6c474d99f38e5340f342ccb8b4f
Ayoub EL Mhamdi <ayoub.elmhamdi@gmail.com>
2023-12-05 07:01:22 +0100

    (patch): pertag

--- config.def.h
+++ config.def.h
@@ -112,4 +112,3 @@ static const Button buttons[] = {
 	{ ClkTagBar,            MODKEY,         Button1,        tag,            {0} },
 	{ ClkTagBar,            MODKEY,         Button3,        toggletag,      {0} },
 };
-
